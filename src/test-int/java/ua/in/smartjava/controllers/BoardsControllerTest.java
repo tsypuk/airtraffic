c6dc806f8065ecb075d2bcd651a9f48c23713034
tsypuk <tsypuk.rb@gmail.com>
2017-01-22 17:52:41 +0200

    Updated controllers. Added h2 for tests

---
+++ src/test-int/java/ua/in/smartjava/controllers/BoardsControllerTest.java
@@ -0,0 +1,13 @@
+package ua.in.smartjava.controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BoardsControllerTest {
+    @Test
+    public void getBoards() throws Exception {
+
+    }
+
+}
